c508f9f87a7f4d26fb04c7dc75c04944246b6db3
BornToLose <twitchplayslore@gmail.com>
2026-03-29 19:06:17 +0200

    AFD overlay

---
+++ overlay/afd/script.js
@@ -0,0 +1,82 @@
+
+function mainLoop(fast) {
+	
+	var interval = fast ? 30000 : 50000;
+	var randWait = fast ? 0 : 10000;
+	
+	//if (fast) {
+		//setTimeout(() => {
+			createImage();
+			$("#imageContainer").fadeTo(5000, 0.75);
+			setTimeout(() => {
+				$("#imageContainer").fadeTo(5000, 0);
+				setTimeout(() => {
+					document.getElementById("imgElement").remove();
+				}, 5000);
+			}, 20000);
+	
+		//}, Math.floor(Math.random() * randWait));
+	//}
+	
+	var nextTimeout = Math.floor(Math.random() * randWait);
+	console.log("Next wait: " + ((interval+nextTimeout-20000)/60000).toFixed(2) + " minutes"); 
+	
+	setInterval(() => {
+		setTimeout(() => {
+			createImage();
+			$("#imageContainer").fadeTo(5000, 0.75);
+			setTimeout(() => {
+				$("#imageContainer").fadeTo(5000, 0);
+				setTimeout(() => {
+					document.getElementById("imgElement").src = "";
+					document.getElementById("imgElement").remove();
+				}, 5000);
+			}, 20000);
+	
+		}, nextTimeout);
+		
+		nextTimeout = Math.floor(Math.random() * randWait);
+		console.log("Next wait: " + ((interval+nextTimeout-20000)/60000).toFixed(2) + " minutes"); 
+		
+	}, interval);
+ 
+} 
+
+function createImage() {
+	
+	var posX = Math.floor(Math.random() * 1771)-50;
+	var posY = Math.floor(Math.random() * 931)-50;
+	var rotAngle = Math.floor(Math.random() * 91)-45;
+	
+	var imgCont = document.getElementById("imageContainer");
+	var imgElem = document.createElement("img");
+	imgElem.id = "imgElement";
+	imgElem.src = "images/" + pickRandomImage();
+	imgElem.style.maxHeight = "300px";
+	imgElem.style.maxWidth = "300px";
+	imgElem.style.height = "auto";
+	imgElem.style.width = "auto";
+	imgCont.style.marginLeft = posX;
+	imgCont.style.marginTop = posY;
+	imgCont.style.marginTop = posY;
+	imgCont.style.rotate = rotAngle + "deg";
+	imgCont.style.opacity = 0;
+	imgCont.appendChild(imgElem);
+	
+}
+
+function pickRandomImage() {
+	
+	const imageList = [
+		"kecleon_red.gif",
+		"kecleon_brown.gif",
+		"kecleon_yellow.gif",
+		"kecleon_green.gif",
+		"kecleon_cyan.gif",
+		"kecleon_blue.gif",
+		"kecleon_purple.gif",
+	];
+	
+	return imageList[Math.floor(Math.random() * imageList.length)];
+	
+}
