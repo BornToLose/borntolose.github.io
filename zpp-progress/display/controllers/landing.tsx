5c599922044ea1a080b01b1ea73df7e741d78857
BornToLose <twitchplayslore@gmail.com>
2026-09-03 12:40:34 +0200

    Quickfix

--- zpp-progress/display/controllers/landing.tsx
+++ zpp-progress/display/controllers/landing.tsx
@@ -23,7 +23,7 @@ namespace TPP.Controllers {
                         <span>Hall of Fame</span>
                     </a>
                     <a href="pokedex.html">
-                        <TPP.Display.Elements.PokeSprite pokemon="Bulbasaur" />
+                        <TPP.Display.Elements.PokeSprite pokemon="Bulbizarre" />
                         <span>Global Pokédex</span>
                     </a>
                 </div>
