4ac4c427f2020ee2cefb5bea23eb606311237926
BornToLose <twitchplayslore@gmail.com>
2026-09-04 20:25:01 +0200

    Updated ZPPX

--- zpp-progress/data/Runs/x.ts
+++ zpp-progress/data/Runs/x.ts
@@ -21,74 +21,75 @@ ZEvent2026.Runs.push(
 		{ Name: "Lépidonille", Time: "0d00h49m17s", Group: "Pokemon" }, 
 		{ Name: "Feuillajou", Time: "0d00h56m32s", Group: "Pokemon" }, 
 		
+		{
+			Group: "Badges", Name: "Badge Coléoptère", Image: "img/badges/bug.png",
+			Time: "2023-04-09T04:11:26.752Z", Attempts: 0
+		},
+			
 		/*{
 			Group: "Trainers", Name: "Sannah", Image: "img/trainers/black/bianca.png",
 			Time: "0d00h08m15s", Attempts: 1
-			
-            {
-                Group: "Badges", Name: "Badge Coléoptère", Image: "img/badges/bug.png",
-                Time: "2023-04-09T04:11:26.752Z", Attempts: 0
-            },
-            {
-                Group: "Badges", Name: "Badge Mur", Image: "img/badges/cliff.png",
-                Time: "2023-04-10T08:27:28.990Z", Attempts: 0
-            },
-            {
-                Group: "Badges", Name: "Badge Lutte", Image: "img/badges/rumble.png",
-                Time: "2023-04-10T15:18:24.214Z", Attempts: 0
-            },
-            {
-                Group: "Badges", Name: "Badge Végétal", Image: "img/badges/plant.png",
-                Time: "2023-04-10T22:13:35.416Z", Attempts: 0
-            },
-            {
-                Group: "Badges", Name: "Badge Tension", Image: "img/badges/voltage.png",
-                Time: "2023-04-11T03:56:44.458Z", Attempts: 0
-            },
-            {
-                Group: "Badges", Name: "Badge Nymphe", Image: "img/badges/fairy.png",
-                Time: "2023-04-11T11:45:49.854Z", Attempts: 0
-            },
-            {
-                Group: "Badges", Name: "Badge Psychisme", Image: "img/badges/psychic.png",
-                Time: "2023-04-12T14:23:27.695Z", Attempts: 0
-            },
-            {
-                Group: "Badges", Name: "Badge Iceberg", Image: "img/badges/iceberg.png",
-                Time: "2023-04-13T12:57:02.597Z", Attempts: 0
-            },
-            {
-                Group: "Elite Four", Name: "Narcisse", Image: "img/trainers/x/siebold.png",
-                Time: "2023-04-14T19:10:54.941Z", Attempts: 0
-            },
-            {
-                Group: "Elite Four", Name: "Malva", Image: "img/trainers/x/malva.png",
-                Time: "2023-04-14T21:40:33.924Z", Attempts: 0
-            },
-            {
-                Group: "Elite Four", Name: "Dracéna", Image: "img/trainers/x/drasna.png",
-                Time: "2023-04-14T18:57:32.205Z", Attempts: 0
-            },
-            {
-                Group: "Elite Four", Name: "Thyméo", Image: "img/trainers/x/wikstrom.png",
-                Time: "2023-04-14T21:08:48.045Z", Attempts: 0
-            },
-            {
-                Group: "Champions", Name: "Dianthéa", Image: "img/trainers/x/diantha.png",
-                Time: "2023-04-15T16:25:02.067Z", Attempts: 0
-            },
-            <TPP.HallOfFame>{
-                Group: "Hall of Fame", Name: "Hall of Fame", Time: "2023-04-15T16:26:22.629Z", Attempts: 0, Party: [
-                    { Pokemon: "Yveltal", Nickname: "SEPHIROTH", Level: 97 },
-                    { Pokemon: "Greninja", Nickname: "!!iyy\u00e9\u00e9\u00e9roff", Level: 98, Gender: "Male" },
-                    { Pokemon: "Electivire", Nickname: "EleSHha!A\ue08e\ue08e", Level: 100, Gender: "Male" },
-                    { Pokemon: "Magmortar", Nickname: "Magtwo", Level: 99, Gender: "Female" },
-                    { Pokemon: "Pinsir", Nickname: "PIKACHU", Level: 95, Gender: "Female" },
-                    { Pokemon: "Dragalge", Nickname: "MadeInAbyss\ue092", Level: 99, Gender: "Female" },
-                ],
-                FirstAttemptDate: "2023-04-14T18:47:03.177Z",
-                Image: "img/ribbons/champion-kalos.png"
-            },
+		},
+		
+		{
+			Group: "Badges", Name: "Badge Mur", Image: "img/badges/cliff.png",
+			Time: "2023-04-10T08:27:28.990Z", Attempts: 0
+		},
+		{
+			Group: "Badges", Name: "Badge Lutte", Image: "img/badges/rumble.png",
+			Time: "2023-04-10T15:18:24.214Z", Attempts: 0
+		},
+		{
+			Group: "Badges", Name: "Badge Végétal", Image: "img/badges/plant.png",
+			Time: "2023-04-10T22:13:35.416Z", Attempts: 0
+		},
+		{
+			Group: "Badges", Name: "Badge Tension", Image: "img/badges/voltage.png",
+			Time: "2023-04-11T03:56:44.458Z", Attempts: 0
+		},
+		{
+			Group: "Badges", Name: "Badge Nymphe", Image: "img/badges/fairy.png",
+			Time: "2023-04-11T11:45:49.854Z", Attempts: 0
+		},
+		{
+			Group: "Badges", Name: "Badge Psychisme", Image: "img/badges/psychic.png",
+			Time: "2023-04-12T14:23:27.695Z", Attempts: 0
+		},
+		{
+			Group: "Badges", Name: "Badge Iceberg", Image: "img/badges/iceberg.png",
+			Time: "2023-04-13T12:57:02.597Z", Attempts: 0
+		},
+		{
+			Group: "Elite Four", Name: "Narcisse", Image: "img/trainers/x/siebold.png",
+			Time: "2023-04-14T19:10:54.941Z", Attempts: 0
+		},
+		{
+			Group: "Elite Four", Name: "Malva", Image: "img/trainers/x/malva.png",
+			Time: "2023-04-14T21:40:33.924Z", Attempts: 0
+		},
+		{
+			Group: "Elite Four", Name: "Dracéna", Image: "img/trainers/x/drasna.png",
+			Time: "2023-04-14T18:57:32.205Z", Attempts: 0
+		},
+		{
+			Group: "Elite Four", Name: "Thyméo", Image: "img/trainers/x/wikstrom.png",
+			Time: "2023-04-14T21:08:48.045Z", Attempts: 0
+		},
+		{
+			Group: "Champions", Name: "Dianthéa", Image: "img/trainers/x/diantha.png",
+			Time: "2023-04-15T16:25:02.067Z", Attempts: 0
+		},
+		<TPP.HallOfFame>{
+			Group: "Hall of Fame", Name: "Hall of Fame", Time: "2023-04-15T16:26:22.629Z", Attempts: 0, Party: [
+				{ Pokemon: "Yveltal", Nickname: "SEPHIROTH", Level: 97 },
+				{ Pokemon: "Greninja", Nickname: "!!iyy\u00e9\u00e9\u00e9roff", Level: 98, Gender: "Male" },
+				{ Pokemon: "Electivire", Nickname: "EleSHha!A\ue08e\ue08e", Level: 100, Gender: "Male" },
+				{ Pokemon: "Magmortar", Nickname: "Magtwo", Level: 99, Gender: "Female" },
+				{ Pokemon: "Pinsir", Nickname: "PIKACHU", Level: 95, Gender: "Female" },
+				{ Pokemon: "Dragalge", Nickname: "MadeInAbyss\ue092", Level: 99, Gender: "Female" },
+			],
+			FirstAttemptDate: "2023-04-14T18:47:03.177Z",
+			Image: "img/ribbons/champion-kalos.png"
 		},*/
 
         ]
