c5a9799b5f8693014b384a5ab3fa7b5e73062f49
BornToLose <twitchplayslore@gmail.com>
2026-09-05 00:29:00 +0200

    Updated ZPPX

---
+++ zpp-progress/data/Intermissions/balatro.ts
@@ -0,0 +1,17 @@
+/// <reference path="../tpp-data.ts" />
+ZEvent2026.Runs.push(
+    {
+        RunName: "Balatro",
+        ColorPrimary: "#ffebec",
+        ColorSecondary: "#bb0d04",
+        StartDate: "2026-09-05T00:00:00Z",
+        Duration: "52h",
+        HostName: "",
+        HostImage: "",
+        // HostImageSource: "",
+		AlwaysShowHOF,
+        Events: [
+
+        ]
+    }
+);
